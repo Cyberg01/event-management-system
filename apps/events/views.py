f76e0d3462b668720adcfbef9040fc35588f0aff
Ananta Alwy <ananta.alwy01@gmail.com>
2026-04-14 17:46:31 +0800

    refactor(Event): update import statement for EventSerializer and maintain code structure

--- apps/events/views.py
+++ apps/events/views.py
@@ -1,10 +1,10 @@
+from .serializers import EventSerializer
+from .models import Event
 from django.shortcuts import get_object_or_404, render
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
-
-from apps.events.serializers import EventSerializer
 
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
@@ -14,3 +14,13 @@ def createEvent(request):
         serializer.save(creatorId=request.user.id)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def showEventById(request, event_id):
+    try:
+        event = get_object_or_404(Event, id=event_id)
+        serializer = EventSerializer(event)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    except Event.DoesNotExist:
+        return Response({'error': 'Event not found'}, status=status.HTTP_404_NOT_FOUND)   
