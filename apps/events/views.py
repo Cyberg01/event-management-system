6767562fcb4e8f45ba9fc3a10bf6919c3c29b29f
Ananta Alwy <ananta.alwy01@gmail.com>
2026-04-14 17:43:53 +0800

    feat(Event): implement createEvent view for event creation with authentication

--- apps/events/views.py
+++ apps/events/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from apps.events.serializers import EventSerializer
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def createEvent(request):
+    serializer = EventSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save(creatorId=request.user.id)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
