ba89cce6515c1e877e8082f4ab4c358b8aea9970
Ananta Alwy <ananta.alwy01@gmail.com>
2026-04-14 13:37:08 +0800

    feat: add user authentication endpoints and update URL routing

---
+++ apps/users/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from apps.users import views
+
+urlpatterns = [
+    path('v1/auth/register/', views.createUser, name='register'),
+    path('v1/auth/profile/', views.showUser, name='profile'),
+    path('v1/auth/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('v1/auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
