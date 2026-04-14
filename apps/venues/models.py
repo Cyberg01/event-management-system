58b48d52a3237367bca958559c681b6977e45247
Ananta Alwy <ananta.alwy01@gmail.com>
2026-04-14 22:31:25 +0800

    feat(Venue): define Venue model with fields for venue details and metadata

--- apps/venues/models.py
+++ apps/venues/models.py
@@ -1,3 +1,21 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+class Venue(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    address = models.TextField(null=False, blank=True)
+    city = models.CharField(max_length=100, null=True, blank=True)
+    state = models.CharField(max_length=100, null=True, blank=True)
+    country = models.CharField(max_length=100, null=True, blank=True)
+    capacity = models.IntegerField(null=True, blank=True)
+    metadata = models.JSONField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'Venue'
+
+    def __str__(self):
+        return self.name
