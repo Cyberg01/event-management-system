ee15e9719fceb0d65dd10bb6758ab850a10d20b0
Ananta Alwy <ananta.alwy01@gmail.com>
2026-04-14 11:34:23 +0800

    feat: :sparkles: implement UserProfile model with role and timestamps

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class UserProfile(AbstractUser):
+    ROLE_CHOICES = (
+        ("ADMIN", "Admin"),
+        ("ATTENDEE", "Attendee"),
+    )
+
+    role = models.CharField(
+        max_length=20,
+        choices=ROLE_CHOICES,
+        default="ATTENDEE"
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.username
