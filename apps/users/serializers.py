52bd3f5f810b9f23f91a78ec4728023b81a32f03
Ananta Alwy <ananta.alwy01@gmail.com>
2026-04-14 13:34:35 +0800

    feat: add user registration and detail serializers

---
+++ apps/users/serializers.py
@@ -0,0 +1,28 @@
+from rest_framework import serializers
+from apps.users.models import UserProfile
+from django.contrib.auth.password_validation import validate_password
+
+class UserRegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    password2 = serializers.CharField(write_only=True, required=True, label="Confirm Password")
+
+    class Meta:
+        model = UserProfile
+        fields = ('id', 'username', 'email', 'password', 'password2', 'first_name', 'last_name', 'role')
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError({"password": "Password fields didn't match."})
+        return attrs
+
+    def create(self, validated_data):
+        validated_data.pop('password2')
+        user = UserProfile.objects.create_user(**validated_data)
+        return user
+
+
+class UserDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserProfile
+        fields = ('id', 'username', 'email', 'first_name', 'last_name', 'role', 'created_at')
+        read_only_fields = ('id', 'created_at')
