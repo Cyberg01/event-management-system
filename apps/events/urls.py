30681342ab1a4d0f4114b90241761f5124ba9855
Ananta Alwy <ananta.alwy01@gmail.com>
2026-04-15 10:56:45 +0800

    fix(urls): change event_id parameter type from int to uuid for event-related endpoints

--- apps/events/urls.py
+++ apps/events/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('v1/events/create/', views.createEvent, name='create_event'),
-    path('v1/events/<int:event_id>/', views.showEventById, name='show_event'),
-    path('v1/events/<int:event_id>/update/', views.updateEvent, name='update_event'),
-    path('v1/events/<int:event_id>/delete/', views.deleteEvent, name='delete_event'),
+    path('v1/events/<uuid:event_id>/', views.showEventById, name='show_event'),
+    path('v1/events/<uuid:event_id>/update/', views.updateEvent, name='update_event'),
+    path('v1/events/<uuid:event_id>/delete/', views.deleteEvent, name='delete_event'),
 ]
