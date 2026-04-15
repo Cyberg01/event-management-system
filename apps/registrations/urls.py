63d5e13d6006ab494524e4185d647cd93935e6a9
Ananta Alwy <ananta.alwy01@gmail.com>
2026-04-15 11:37:30 +0800

    fix(urls): comment out unused registration list endpoint

--- apps/registrations/urls.py
+++ apps/registrations/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('v1/registrations/', views.listRegistrations, name='list_registrations'),
+    # path('v1/registrations/', views.listRegistrations, name='list_registrations'),
     path('v1/registrations/create/', views.createRegistration, name='create_registration'),
     path('v1/registrations/<uuid:registration_id>/', views.showRegistrationById, name='show_registration'),
     path('v1/registrations/<uuid:registration_id>/update/', views.updateRegistration, name='update_registration'),
