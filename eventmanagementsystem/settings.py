3f6af7cce212740a9e8ae5c1ac3ede2f90ef4fe0
Ananta Alwy <ananta.alwy01@gmail.com>
2026-04-15 15:46:01 +0800

    refactor(settings): add django_filters and configure default filter backends in REST framework

--- eventmanagementsystem/settings.py
+++ eventmanagementsystem/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
     'drf_spectacular',
     'corsheaders',
+    'django_filters',
 
     # apps
     'apps.users',
@@ -145,6 +146,11 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    'DEFAULT_FILTER_BACKENDS': [
+      'django_filters.rest_framework.DjangoFilterBackend',
+      'rest_framework.filters.SearchFilter',
+      'rest_framework.filters.OrderingFilter',
+    ]
 }
 
 # Simple JWT Configuration
