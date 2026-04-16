4a52783729549cd8325d3ce5e96ad89447a0cc0d
Ananta Alwy <ananta.alwy01@gmail.com>
2026-04-17 00:38:26 +0800

    fix(auth): update URL patterns to remove trailing slashes for authentication endpoints

--- apps/auth/urls.py
+++ apps/auth/urls.py
@@ -6,7 +6,7 @@ app_name = "auth"
 
 urlpatterns = [
     # Endpoint authenticationusing JWT
-    path("v1/auth/login/", views.CustomTokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("v1/auth/refresh/", views.CustomTokenRefreshView.as_view(), name="token_refresh"),
-    path("v1/auth/logout/", views.logout_view, name="logout"),
+    path("v1/auth/login", views.CustomTokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("v1/auth/refresh", views.CustomTokenRefreshView.as_view(), name="token_refresh"),
+    path("v1/auth/logout", views.logout_view, name="logout"),
 ]
