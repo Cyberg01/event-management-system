41ab0281699d933270de58e3f62e8f5959aebf18
Ananta Alwy <ananta.alwy01@gmail.com>
2026-04-16 13:16:47 +0800

    feat(filters): add VenueFilter for filtering venue queries

---
+++ apps/venues/filters.py
@@ -0,0 +1,14 @@
+import django_filters
+from .models import Venue
+
+
+class VenueFilter(django_filters.FilterSet):
+  name = django_filters.CharFilter(field_name="name", lookup_expr="icontains")
+  city = django_filters.CharFilter(field_name="city", lookup_expr="icontains")
+  state = django_filters.CharFilter(field_name="state", lookup_expr="icontains")
+  country = django_filters.CharFilter(field_name="country", lookup_expr="icontains")
+  capacity = django_filters.NumberFilter(field_name="capacity", lookup_expr="gte")
+
+  class Meta:
+    model = Venue
+    fields = ('name', 'city', 'state', 'country', 'capacity')
