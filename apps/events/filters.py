4fe3e758fb8d35e67bbef52c93d92f62ff9381e5
Ananta Alwy <ananta.alwy01@gmail.com>
2026-04-16 11:15:53 +0800

    refactor(events): update EventFilter to enhance date filtering and venue search capabilities

--- apps/events/filters.py
+++ apps/events/filters.py
@@ -5,9 +5,16 @@ from apps.events.models import Event
 class EventFilter(django_filters.FilterSet):
     status = django_filters.CharFilter(field_name="status", lookup_expr="iexact")
     event_type = django_filters.CharFilter(field_name="event_type", lookup_expr="iexact")
-    start_date = django_filters.DateTimeFilter(field_name="start_date", lookup_expr="gte")
-    end_date = django_filters.DateTimeFilter(field_name="end_date", lookup_expr="lte")
-    capacity = django_filters.NumberFilter(field_name="capacity", lookup_expr="gte")
+    event_start_date = django_filters.DateTimeFilter(field_name="event_start_time", lookup_expr="gte")
+    event_end_date = django_filters.DateTimeFilter(field_name="event_end_time", lookup_expr="lte")
+    registration_start_date = django_filters.DateTimeFilter(field_name="registration_start_time", lookup_expr="gte")
+    registration_end_date = django_filters.DateTimeFilter(field_name="registration_end_time", lookup_expr="lte")
+    
+    """venue filter"""
+    venue_name = django_filters.CharFilter(field_name="venue__name", lookup_expr="icontains")
+    venume_city = django_filters.CharFilter(field_name="venue__city", lookup_expr="icontains")
+    venue_state = django_filters.CharFilter(field_name="venue__state", lookup_expr="icontains")
+    venue_country = django_filters.CharFilter(field_name="venue__country", lookup_expr="icontains")
 
     class Meta:
         model = Event
