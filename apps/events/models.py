41a8077dfabb54833c259d8a9099d0e07f2fc73b
Ananta Alwy <ananta.alwy01@gmail.com>
2026-04-16 11:31:40 +0800

    refactor(events): remove unused imports from models.py

--- apps/events/models.py
+++ apps/events/models.py
@@ -1,7 +1,5 @@
 import uuid
 from django.db import models
-from django.utils import timezone
-from django.core.exceptions import ValidationError
 from django.conf import settings
 
 EVENT_STATUS = (
